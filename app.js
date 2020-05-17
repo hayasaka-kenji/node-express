833f5febddc13275a501c3388f110ae30e6cec79
hayasaka-kenji <kenji.hayasaka@imjp.co.jp>
2020-05-17 20:28:30 +0900

    Learned ejs-basic

--- app.js
+++ app.js
@@ -3,14 +3,15 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-// // pug (views内のpugを指定)
-// app.get('/', (req, res) => {
-//   res.status(200).render('index.pug', {title: 'Node.js app'})
-// });
-
-// ejs
 app.get('/', (req, res) => {
-  res.status(200).render('index.ejs', {title: 'Node.js app'})
+  const data = {
+    items: [
+      {name: 'Bob'},
+      {name: 'John'},
+      {name: 'Michael'},
+    ]
+  }
+  res.status(200).render('index.ejs', data)
 });
 
 
