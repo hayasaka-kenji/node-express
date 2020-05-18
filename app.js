b2efbf7164e04a0ca8df04e84ae0191361fa6e2e
hayasaka-kenji <kenji.hayasaka@imjp.co.jp>
2020-05-18 15:31:05 +0900

    Learned header

--- app.js
+++ app.js
@@ -4,17 +4,12 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-
 app.get('/', (req, res) => {
-  res.render('./index') // .ejsの表記は省略可能
-});
-
-// indexからpostで送る。
-// debugからreq.bodyで送った値を見ることができる。もしくはreqにカーソルを当てる
-app.post('/', (req, res) => {
-  res.send('OK')
+  console.log(req.get('user-agent'));
+  // クラアントにキャッシュさせない
+  res.set('cash-control', 'no-cache');
+  res.set('Pragma', 'no-cache');
+  res.render('./index')
 });
 
 app.listen(3000);
