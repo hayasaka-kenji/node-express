863da8cbb7d2f1ca11f9dfffd0b1dfddda57319e
hayasaka-kenji <kenji.hayasaka@imjp.co.jp>
2020-05-26 08:51:04 +0900

    laced a static file.

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+
+app.set('view engine', 'ejs');
+app.disable('x-powerd-by');
+
+app.use('/public', express.static(__dirname + '/public/' + (process.env.NODE_ENV === 'development' ? 'development' : 'production')));
+
+app.use('/', require('./routes/index'));
+
+app.listen(3000);
