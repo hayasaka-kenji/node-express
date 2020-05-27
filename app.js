96ca6cb2bcd4f0c5afc724ff04a56f4f158bb199
hayasaka-kenji <kenji.hayasaka@imjp.co.jp>
2020-05-27 20:12:59 +0900

    Learned 'create article'

--- app.js
+++ app.js
@@ -8,13 +8,14 @@ app.set('view engine', 'ejs');
 app.disable('x-powerd-by');
 
 app.use('/public',
-  express.static(__dirname + '/public/' + 
+  express.static(__dirname + '/public/' +
     (process.env.NODE_ENV === 'development' ? 'development' : 'production'))
 );
 
 app.use(accesslogger());
 
 app.use('/', require('./routes/index'));
+app.use('/posts/', require('./routes/posts'));
 
 app.use(systemlogger());
 
