b7e8a1e0579a01fd97792764d86460294a38cb38
hayasaka-kenji <kenji.hayasaka@imjp.co.jp>
2020-05-28 19:12:15 +0900

    Learn 'pagination'

--- routes/search.js
+++ routes/search.js
@@ -1,17 +1,38 @@
 const { CONNECTION_URL, OPTIONS, DATABASE } = require('../config/mongodb.config');
+const { MAX_ITEM_PER_PAGE } = require('../config/app.config').search;
 const router = require('express').Router();
 const MongoClient = require('mongodb').MongoClient;
 
 router.get('/*', (req, res) => {
+  let page = req.query.page ? parseInt(req.query.page) : 1;
   const keyword = req.query.keyword || '';
   const regexp = new RegExp(`.*${keyword}.*`);
+  const query = {
+    $or: [{ title: regexp }, { content: regexp }]
+  };
   MongoClient.connect(CONNECTION_URL, OPTIONS, (error, client) => {
     let db = client.db(DATABASE);
-    db.collection('posts').find({
-      // OR検索を正規表現(部分一致)で実行 
-      $or: [{ title: regexp }, { content: regexp }]
-    }).sort({ published: -1 }).toArray().then((list) => {
-      const data = { keyword, list };
+    // 検索の総件数と検索結果の一部が必要
+    Promise.all([
+      db.collection('posts')
+        .find(query)
+        .count(),
+      db.collection('posts')
+        .find(query)
+        .sort({ published: -1 })
+        .skip((page - 1) * MAX_ITEM_PER_PAGE) // ページ番号は1番から始まるので1を引く
+        .limit(MAX_ITEM_PER_PAGE) // 上限を設定
+        .toArray()
+    ]).then((results) => {
+      const data = {
+        keyword,
+        count: results[0],
+        list: results[1],
+        pagination: {
+          max: Math.ceil(results[0] / MAX_ITEM_PER_PAGE),
+          current: page
+        }
+      };
       res.render('./search/list', data);
     }).catch((error) => {
       throw error;
@@ -20,8 +41,5 @@ router.get('/*', (req, res) => {
     });
   });
 });
-
-
-
 
 module.exports = router;
